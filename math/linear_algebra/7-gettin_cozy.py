b23553de673f59974ecc7f199956502ffd79cb96
essioui <salahessioui@gmail.com>
2024-12-05 23:29:57 +0100

    concatenates two matrices

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Defines cat_matrices2D"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    concatenates two matrices along a specific axis
+    Args:
+        mat1: 2D matrices containing ints/floats
+        mat2: 2D matrices containing ints/floats
+    return:
+        None
+        matrix
+    """
+    if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            return mat1 + mat2
+    elif axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        else:
+            return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+    return None
