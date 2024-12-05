1bc1d3d5458eedb2b95437541bd2c77d324657eb
essioui <salahessioui@gmail.com>
2024-12-05 23:35:48 +0100

    cat_matrices2D

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -16,10 +16,10 @@ def cat_matrices2D(mat1, mat2, axis=0):
         if len(mat1) != len(mat2):
             return None
         else:
-            return mat1 + mat2
+            return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     elif axis == 0:
         if len(mat1[0]) != len(mat2[0]):
             return None
         else:
-            return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+            return mat1 + mat2
     return None
