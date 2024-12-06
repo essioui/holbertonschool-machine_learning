867bee93394d5045276a9fd4c1fc182499accd08
essioui <salahessioui@gmail.com>
2024-12-06 15:23:47 +0100

    transpose a matrix

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Defines np_transpose"""
+
+
+def np_transpose(matrix):
+    """
+    transposes a matrix
+    Args:
+        matrix: numpy.ndarray
+    Return:
+        Transpose numpy.ndarray
+    """
+    return matrix.T
