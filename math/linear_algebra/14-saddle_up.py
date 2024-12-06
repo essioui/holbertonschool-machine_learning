ef8fbe7d746465851def388a2a75f28e80e22d6b
essioui <salahessioui@gmail.com>
2024-12-06 15:58:43 +0100

    matrix multiplication

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines np_matmul"""
+
+
+def np_matmul(mat1, mat2):
+    """
+    performs matrix multiplication
+    Args:
+        mat1: numpy.ndarray
+        mat2: numpy.ndarray
+    Return:
+        matrix
+    """
+    return mat1 @ mat2
