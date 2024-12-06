f1cdc720a242fdb91549cdf19925c4372314e7be
essioui <salahessioui@gmail.com>
2024-12-06 15:13:13 +0100

    shape of a numpy

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Defines np_shape"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy.ndarray
+    Args:
+        matrix: numpy.ndarray
+    Return:
+        tuple of shape
+    """
+    return matrix.shape
