6135529b48d7ef81cf11ca98ceded7a70daf6560
essioui <salahessioui@gmail.com>
2024-12-06 14:45:04 +0100

    pycode

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -16,6 +16,6 @@ def mat_mul(mat1, mat2):
     result = [[0 for _ in range(len(mat2[0]))] for _ in range(len(mat1))]
     for i in range(len(mat1)):
         for j in range(len(mat2[0])):
-            for l in range(len(mat2)):
-                result[i][j] += mat1[i][l]*mat2[l][j]
+            for k in range(len(mat2)):
+                result[i][j] += mat1[i][k]*mat2[k][j]
     return result
