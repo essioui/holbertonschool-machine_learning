54769aeb6869b334cf42174c9efea007d043d968
essioui <salahessioui@gmail.com>
2024-12-08 22:17:02 +0100

    fixed

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -1,4 +1,4 @@
-#!/usr/bin/ env python3
+#!/usr/bin/env python3
 """Defines cat_matrices"""
 import numpy as np
 
@@ -8,14 +8,11 @@ def cat_matrices(mat1, mat2, axis=0):
     concatenates two matrices along a specific axis
     """
     try:
-        #transfer lists to arrays numpy for use np.conacatenate
+        # transfer lists to arrays numpy for use np.conacatenate
         np_mat1 = np.array(mat1)
         np_mat2 = np.array(mat2)
         result = np.concatenate((np_mat1, np_mat2), axis=axis)
-        #return the result as lists
+        # return the result as lists
         return result.tolist()
     except ValueError:
         return None
-
-
-        
