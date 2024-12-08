2ba885b086810bf73e0326d4b41b02aa9dd2ca99
essioui <salahessioui@gmail.com>
2024-12-08 22:48:01 +0100

    conatenate with axis 1

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -7,9 +7,15 @@ def cat_matrices(mat1, mat2, axis=0):
     concatenates two matrices along a specific axis
     """
     if axis == 0:
+        # concatenate 1D lists
         if isinstance(mat1[0], (int, float)) and isinstance(mat2[0], (int, float)):
             return mat1 +mat2
+        # concatenate 2D lists
         if all(len(row) == len(mat2[0]) for row in mat1) and all(len(row) == len(mat1[0])for row in mat2):
             return mat1+mat2
         return None
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
     return None
