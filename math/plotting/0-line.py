f82687bc0dc624c9cea8fbf5468a11428bac9390
essioui <salahessioui@gmail.com>
2024-12-09 16:56:39 +0100

    ok

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -5,18 +5,27 @@ import matplotlib.pyplot as plt
 
 def line():
     """
-    line graph
-    plt.figure: dimension of figure (width, height)
-    plt.plot(x, y, 'r) plots the x and y values with a red solid line:
-        x = np.arange(0, 11)
-        y = np.arange(0,11) ** 3
-        plt.xlim = the axis x start from 0 end 10
-        r: for color red
-    show(): display the plot
+    line() generates a line plot with the x-axis ranging from 0 to 10 and 
+    the y-axis being the cube of the x values.
+
+    The function:
+    - Initializes x values ranging from 0 to 10 using np.arange(0, 11)
+    - Calculates the y values as the cube of the x values
+    - Creates a figure with specified dimensions
+    - Plots the x and y values as a red solid line
+    - Sets the x-axis limits from 0 to 10 using plt.xlim(0, 10)
+    - Displays the plot using plt.show()
+
+    Returns:
+    None
     """
     y = np.arange(0, 11) ** 3
+    # Create a figure with specific dimensions
     plt.figure(figsize=(6.4, 4.8))
 
+    # Plot the x and y values as a red solid line
     plt.plot(np.arange(0, 11), y, 'r')
+    # Set the x-axis limits to range from 0 to 10
     plt.xlim(0, 10)
+    # Display the plot
     plt.show()
