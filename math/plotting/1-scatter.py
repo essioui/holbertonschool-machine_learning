0fd97811daf1a92eb896fdd43006db612a9425a0
essioui <salahessioui@gmail.com>
2024-12-09 22:37:15 +0100

    pycode

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -16,7 +16,6 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-   # your code here
     plt.xlabel('Height (in)')
     plt.ylabel('Weight (lbs)')
     plt.title("Men's Height vs Weight")
