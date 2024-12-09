73a356c66d278532eb3fa7be57266b878c081e9a
essioui <salahessioui@gmail.com>
2024-12-09 16:51:47 +0100

    fixed documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 def line():
     """
     line graph
-    plt.figure: dimension of figure
-    plt.plot:
+    plt.figure: dimension of figure (width, height)
+    plt.plot(x, y, 'r) plots the x and y values with a red solid line:
         x = np.arange(0, 11)
         y = np.arange(0,11) ** 3
         plt.xlim = the axis x start from 0 end 10
         r: for color red
-    show(): function for see the figure
+    show(): display the plot
     """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
