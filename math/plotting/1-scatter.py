e74c17a252dc9486041fc41175df96883c8e588b
essioui <salahessioui@gmail.com>
2024-12-09 22:28:51 +0100

    scatter plot

---
+++ math/plotting/1-scatter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""scatter plot"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """
+    mean: variance
+    cov: covariance
+    """
+    mean = [69, 0]
+    cov = [[15, 8], [8, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 180
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.suptitle("Men's Height vs Weight")
+    plt.scatter(x, y, c='magenta', marker='o')
+    plt.show()
