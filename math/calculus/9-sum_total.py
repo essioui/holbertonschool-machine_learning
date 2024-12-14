af477ece6f488a53ffbe69d4222d4f8b1bceea80
essioui <salahessioui@gmail.com>
2024-12-14 13:12:21 +0100

    sum total n

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -8,7 +8,7 @@ def summation_i_squared(n):
     """
     if type(n) is not int:
         return None
-    elif n < 0:
+    elif n < 1:
         return None
     else:
         return int((n * (n + 1) * ((2 * n) + 1)) / 6)
