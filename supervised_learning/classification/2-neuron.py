d1a30be758c6eada6771ac0bd447ec5325401051
essioui <salahessioui@gmail.com>
2025-01-03 17:31:25 +0100

    Neuron Forward Propagation

---
+++ supervised_learning/classification/2-neuron.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Privatize Neuron
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Defines a single neuron performing binary classification
+    """
+    def __init__(self, nx):
+        """
+        Constructor build single neuron
+        Args:
+            nx: the number of input features to the neuron
+        Raises:
+            TypeError("nx must be an integer")
+            ValueError("nx must be a positive")
+        Private instance attributes:
+            __W
+            __b
+            __A
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculate the forward propagation
+        Args:
+            X:numpy.ndarray with shape (nx, m) that contains the input data:
+                nx is the number of input features to the neuron
+                m is the number of examples
+        Returns:
+            the private attribute __A
+        """
+        z = np.dot(self.__A, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-z))
+        return self.__A
