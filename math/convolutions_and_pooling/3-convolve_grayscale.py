c4c9a2c2b03373266a4f65bf45a119a494442516
essioui <salahessioui@gmail.com>
2025-02-09 14:00:50 +0100

    convolution same valid tuple

--- math/convolutions_and_pooling/3-convolve_grayscale.py
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -30,24 +30,20 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        ph = ((h - 1) * sh + kh - h) // 2
-        pw = ((w - 1) * sw + kw - w) // 2
-    elif padding == 'valid':
-        ph, pw = 0, 0
-    else:
-        ph, pw = padding
-
-    images_padding = np.pad(images, ((0, 0), (ph, ph),
-                                     (pw, pw)), 'constant', constant_values=0)
-
-    new_height = ((h + 2 * ph - kh) // sh) + 1
-    new_width = ((w + 2 * pw - kw) // sw) + 1
-
-    convolution = np.zeros((m, new_height, new_width))
-
-    for i in range(new_height):
-        for j in range(new_width):
-            region = images_padding[:, i * sh:i * sh + kh, j * sw:j * sw + kw]
-
-            convolution[:, i, j] = np.sum(region * kernel, axis=(1, 2))
-    return convolution
+        new_heigt = (((h - 1) * sh + kh - h) // 2) + 1
+        new_width = (((w - 1) * sw + kw - w) // 2) + 1
+    if padding == 'valid':
+        new_heigt, new_width = 0, 0
+    if type(padding) is tuple:
+        new_heigt, new_width = padding
+    imgh, imgw = (h-kh+2*new_heigt)//sh + 1, (w-kw+2*new_width)//sw + 1
+    output = np.zeros((m, imgh, imgw))
+    new = np.pad(images, ((0, 0), (new_heigt, new_heigt),
+                          (new_width, new_width)), 'constant')
+    for i in range(imgh):
+        for j in range(imgw):
+            output[:, i, j] = np.tensordot(new[:,
+                                           i*sh:i*sh+kh,
+                                           j*sw:j*sw+kw],
+                                           kernel)
+    return output
