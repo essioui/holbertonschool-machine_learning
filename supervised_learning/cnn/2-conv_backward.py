12f0810d54c78b975252f64268df49f57b5a756b
essioui <salahessioui@gmail.com>
2025-02-14 01:13:00 +0100

    backward prop

--- supervised_learning/cnn/2-conv_backward.py
+++ supervised_learning/cnn/2-conv_backward.py
@@ -23,56 +23,40 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
             kw is the filter width
         b is a numpy.ndarray of shape (1, 1, 1, c_new)
         padding is a string that is either same or valid
-        stride is a tuple of (sh, sw)
+        stride is a tuple of (sh,sw)
             sh is the stride for the height
             sw is the stride for the width
     Returns:
         the partial derivatives dA_prev, dW, db
     """
-    m, h_new, w_new, c_new = dZ.shape
-    m, h_prev, w_prev, c_prev = A_prev.shape
-    kh, kw, _, _ = W.shape
+    m, imgh, imgw, c = dZ.shape
+    kh, kw, kc, knc = W.shape
     sh, sw = stride
+    imghp, imgwp = 0, 0
+    if padding == 'same':
+        imghp = (((imgh * sh) - sh + kh - imgh) // 2) + 1
+        imgwp = (((imgw * sw) - sw + kw - imgw) // 2) + 1
+    if isinstance(padding, tuple):
+        imghp, imgwp = padding
 
-    if padding == "same":
-        pad_h = max((h_prev - 1) * sh + kh - h_prev, 0)
-        pad_w = max((w_prev - 1) * sw + kw - w_prev, 0)
-        pad_top = pad_h // 2
-        pad_bottom = pad_h - pad_top
-        pad_left = pad_w // 2
-        pad_right = pad_w - pad_left
-        A_prev_pad = np.pad(A_prev, ((0,), (pad_top,),
-                                     (pad_left,), (0,)), mode='constant')
-    else:
-        pad_top = pad_bottom = pad_left = pad_right = 0
-        A_prev_pad = A_prev
-
+    new = np.pad(A_prev, ((0, 0), (imghp, imghp),
+                          (imgwp, imgwp), (0, 0)),
+                 'constant', constant_values=0)
     db = np.sum(dZ, axis=(0, 1, 2), keepdims=True)
-
-    dW = np.zeros(W.shape)
-
-    dA_prev_pad = np.pad(np.zeros(A_prev.shape), ((0,), (pad_top,),
-                                                  (pad_bottom,), (pad_left,),
-                                                  (pad_right,), (0,)),
-                         mode='constant')
-    for i in range(m):
-        for h in range(h_new):
-            for w in range(w_new):
-                for c in range(c_new):
-                    vert_start = h * sh
-                    vert_end = vert_start + kh
-                    horiz_start = w * sw
-                    horiz_end = horiz_start + kw
-
-                    dW[:, :, :, c] += A_prev_pad[i, vert_start:vert_end,
-                                                 horiz_start:horiz_end,
-                                                 :] * dZ[i, h, w, c]
-
-                    dA_prev_pad[i, vert_start:vert_end,
-                                horiz_start:horiz_end,
-                                :] += W[:, :, :, c] * dZ[i, h, w, c]
-
-    dA_prev = dA_prev_pad[:, pad_top:pad_top+h_prev,
-                          pad_left:pad_left+w_prev, :]
-
-    return dA_prev, dW, db
+    newDZ = np.zeros(new.shape)
+    dW = np.zeros_like(W)
+    for n in range(m):
+        for i in range(imgh):
+            for j in range(imgw):
+                for k in range(knc):
+                    newDZ[n,
+                          i*sh:i*sh+kh,
+                          j*sw:j*sw+kw, :] += np.multiply(dZ[n, i, j, k],
+                                                          W[..., k])
+                    dW[..., k] += np.multiply(dZ[n, i, j, k],
+                                              new[n,
+                                                  i*sh:i*sh+kh,
+                                                  j*sw:j*sw+kw, :])
+    if padding == 'same':
+        newDZ = newDZ[:, imghp:-imghp, imgwp:-imgwp, :]
+    return newDZ, dW, db
