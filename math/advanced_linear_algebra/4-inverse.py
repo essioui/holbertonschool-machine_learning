97e72d400c3143ad32e619a38e4e7a2655aa40a8
essioui <salahessioui@gmail.com>
2025-03-22 21:00:33 +0100

    inversed matrix

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -15,10 +15,10 @@ def inverse(matrix):
         raise TypeError("matrix must be a list of lists")
 
     if len(matrix) == 0:
-        raise ValueError("matrix must be a non-empty square matrix")
+        raise ValueError("matrix must be a list of lists")
 
     if any(len(row) != len(matrix) for row in matrix):
-        raise TypeError("matrix must be a non-empty square matrix")
+        raise ValueError("matrix must be a non-empty square matrix")
 
     det = determinant(matrix)
 
