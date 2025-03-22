03780fbe60d62c78ceb3563c46fbaf9d09ce5a3b
essioui <salahessioui@gmail.com>
2025-03-22 21:15:23 +0100

    defini

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -24,9 +24,6 @@ def definiteness(matrix):
     if matrix.shape[0] != matrix.shape[1]:
         return None
 
-    if not np.array_equal(matrix, matrix.T):
-        return "matrix is not symmetric"
-
     eigenvalues = np.linalg.eigvals(matrix)
 
     if np.all(eigenvalues > 0):
