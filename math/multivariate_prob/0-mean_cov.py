09c79a0cf70920ab3eb2a06645ef26f510896009
essioui <salahessioui@gmail.com>
2025-03-28 03:21:44 +0100

    mean covariance

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Module define Mean and Covariance
+"""
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    Calculates the mean and covariance
+    Args:
+        n is the number of data points
+        d is the number of dimensions in each data point
+    Returns: mean, cov:
+        mean is a numpy.ndarray of shape (1, d)
+        cov is a numpy.ndarray of shape (d, d)
+    """
+    n, d = X.shape
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+
+    mean = np.mean(X, axis=0, keepdims=True)
+
+    X_centrer = X - mean
+
+    covariance = X_centrer.T @ X_centrer / (n - 1)
+
+    return mean, covariance
