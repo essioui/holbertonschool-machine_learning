7c9aef24d3b2f210a8bf65caa9fef2576339740b
essioui <salahessioui@gmail.com>
2025-03-29 20:26:50 +0100

    PCA

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -9,11 +9,9 @@ def pca(X, var=0.95):
     """
     Perform PCA on the dataset X and return the weights matrix W
     that maintains the specified fraction of the variance.
-    
     Parameters:
     X: numpy.ndarray of shape (n, d), the dataset to perform PCA on
     var: float, the fraction of variance to retain (default is 0.95)
-    
     Returns:
     W: numpy.ndarray of shape (d, nd), the weights matrix
     """
