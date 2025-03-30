d001f624836e5f5f4b465e51b4fb09acca175fd8
essioui <salahessioui@gmail.com>
2025-03-30 02:45:48 +0100

    Initialize t-SNE

---
+++ unsupervised_learning/dimensionality_reduction/2-P_init.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+"""
+import numpy as np
+
+
+def P_init(X: np.ndarray, perplexity: float):
+    """
+    Initializes all variables required to calculate the P affinities in t-SNE
+    Args:
+        X is a numpy.ndarray of shape (n, d)
+            n is the number of data points
+            d is the number of dimensions in each poin
+        perplexity is the perplexity that all Gaussian distributions
+    Returns:
+        (D, P, betas, H
+            D: a numpy.ndarray of shape (n, n)
+            The diagonal of D should be 0s
+            P: a numpy.ndarray of shape (n, n) initialized to all 0
+            betas: a numpy.ndarray of shape (n, 1) initialized to all 1
+    """
+    n, d = X.shape
+
+    sum_X = np.sum(np.square(X), axis=1, keepdims=True)
+
+    D = sum_X + sum_X.T - 2 * np.dot(X, X.T)
+
+    np.fill_diagonal(D, 0)
+
+    P = np.zeros((n, n))
+
+    betas = np.ones((n, 1))
+
+    H = np.log2(perplexity)
+
+    return D, P, betas, H
