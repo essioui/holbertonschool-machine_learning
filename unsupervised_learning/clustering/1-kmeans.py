1a25114da807be032a8c22a66add1b20d108e09d
essioui <salahessioui@gmail.com>
2025-04-03 16:45:23 +0100

    kmeans algorithms

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -1,35 +1,44 @@
 #!/usr/bin/env python3
-"""
-Module define K-means
-"""
+"""Clustering module"""
 import numpy as np
 
 
-def kmeans(X, k, iterations=1000):
-    """
-    Performs K-means on a dataset
-    Args:
-        X is a numpy.ndarray of shape (n, d)
-            n is the number of data points
-            d is the number of dimensions
-        k is a positive integer containing the number of clusters
-        iterations is a positive integer containing the maximum of iterations
+def get_closest(X, centroids):
+    """Finds the closest points to centroids
+
+    Arguments:
+        X {np.ndarray} -- Containing the dataset
+        centroids {np.ndarray} -- Containing the centroids in each dimension
+
     Returns:
-        C, clss, or None, None on failure
-            C is a numpy.ndarray of shape (k, d)
-            clss is a numpy.ndarray of shape (n,)
+        np.ndarray -- Containing the index of the nearest centroid
+    """
+    distances = np.sqrt(np.sum((X - centroids[:, np.newaxis])**2, axis=2))
+    return np.argmin(distances, axis=0)
+
+
+def kmeans(X, k, iterations=1000):
+    """Performs the K-means algorithm
+
+    Arguments:
+        X {np.ndarray} -- Containing the data set
+        k {int} -- Is the number of clusters
+
+    Keyword Arguments:
+        iterations {int} -- Is the number of iterations (default: {1000})
+
+    Returns:
+        np.ndarray, np.ndarray -- The newly moved centroids, and the newly
+        assigned classes
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-
-    if not isinstance(k, int) or k <= 0 or k > X.shape[0]:
+    if not isinstance(k, int) or k < 1:
+        return None, None
+    if not isinstance(iterations, int) or iterations < 1:
         return None, None
 
-    if not isinstance(iterations, int) or iterations <= 0:
-        return None, None
-
-    _, d = X.shape
-
+    n, d = X.shape
     centroids = np.random.uniform(
         low=np.min(X, axis=0),
         high=np.max(X, axis=0),
@@ -38,22 +47,18 @@ def kmeans(X, k, iterations=1000):
 
     for i in range(iterations):
         old_centroids = np.copy(centroids)
-
-        distances = np.sqrt(np.sum((X[:, np.newaxis] - centroids)**2, axis=2))
-
-        clss = np.argmin(distances, axis=1)
+        closest = get_closest(X, centroids)
 
         for j in range(k):
-            if X[clss == j].size == 0:
+            if (X[closest == j].size == 0):
                 centroids[j] = np.random.uniform(
                     low=np.min(X, axis=0),
                     high=np.max(X, axis=0),
                     size=(1, d)
                 )
             else:
-                centroids[j] = np.mean(X[clss == j], axis=0)
-
-        if np.allclose(old_centroids, centroids):
+                centroids[j] = np.mean(X[closest == j], axis=0)
+        if np.all(old_centroids == centroids):
             break
 
-    return centroids, clss
+    return centroids, get_closest(X, centroids)
