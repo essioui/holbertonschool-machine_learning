f4c5f2c5c89d7900e07780ae8b9fa9f4f7bc14c8
essioui <salahessioui@gmail.com>
2025-04-05 00:00:26 +0100

    maximization

---
+++ unsupervised_learning/clustering/7-maximization.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Clustering module
+"""
+import numpy as np
+
+
+def maximization(X, g):
+    """
+    Calculates the maximization step in the EM algorithm for a GMM
+    Args:
+        X is a numpy.ndarray of shape (n, d)
+        g is a numpy.ndarray of shape (k, n)
+    Returns:
+        pi, m, S, or None, None, None
+            pi is a numpy.ndarray of shape (k,)
+            m is a numpy.ndarray of shape (k, d)
+            S is a numpy.ndarray of shape (k, d, d)
+    """
+    if not isinstance(X, np.ndarray) or not isinstance(g, np.ndarray):
+        return None, None, None
+
+    if len(X.shape) != 2 or len(g.shape) != 2:
+        return None, None, None
+
+    n, d = X.shape
+    k, n2 = g.shape
+
+    if n != n2:
+        return None, None, None
+
+    Nk = np.sum(g, axis=1)
+
+    pi = Nk / n
+
+    m = np.dot(g, X) / Nk[:, np.newaxis]
+
+    S = np.zeros((k, d, d))
+    for i in range(k):
+        x_shifted = X - m[i]
+        weighted = g[i][:, np.newaxis] * x_shifted
+        S[i] = np.dot(weighted.T, x_shifted) / Nk[i]
+
+    return pi, m, S
