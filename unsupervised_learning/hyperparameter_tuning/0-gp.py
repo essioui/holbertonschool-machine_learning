b0e4401e4608ebf9b5594f1ecfffab3491a3d979
essioui <salahessioui@gmail.com>
2025-04-16 15:55:01 +0100

    hyperparameter

---
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module define hyperparameter
+"""
+class GaussianProcess:
+    """
+    Represents a noiseless 1D Gaussian process
+    """
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Class constructor
+        Args:
+        """
