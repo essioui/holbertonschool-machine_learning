d6c2d10f7d290bc8e83381b8f08b771178ea4bd2
essioui <salahessioui@gmail.com>
2025-04-17 15:38:15 +0100

    Initialize Gaussian Process

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -2,12 +2,37 @@
 """
 Module define hyperparameter
 """
+import numpy as np
+
+
 class GaussianProcess:
     """
     Represents a noiseless 1D Gaussian process
     """
+
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
         """
         Class constructor
-        Args:
         """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix,
+        the kernel should use the Radial Basis Function (RBF)
+        Args:
+            X1 is a numpy.ndarray of shape (m, 1)
+            X2 is a numpy.ndarray of shape (n, 1
+        Returns:
+            the covariance kernel matrix as a numpy.ndarray of shape (m, n)
+        """
+        sqdist = (
+            np.sum(X1**2, 1).reshape(-1, 1) +
+            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
+        )
+        return self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * sqdist)
