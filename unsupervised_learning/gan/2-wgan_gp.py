ac6ba0d1acf6537b1a0969f3c17f2955ac452b88
essioui <salahessioui@gmail.com>
2025-05-02 15:55:19 +0100

    Wasserstein GANs with gradient penalty

--- unsupervised_learning/gan/2-wgan_gp.py
+++ unsupervised_learning/gan/2-wgan_gp.py
@@ -6,72 +6,94 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-class WGAN_GP(keras.Model) :    
-    def __init__( self, generator , discriminator , latent_generator,
+
+class WGAN_GP(keras.Model):
+    """
+    This class represents a Wasserstein GAN (WGAN) with weight clipping.
+    """
+
+    def __init__(self, generator, discriminator, latent_generator,
                  real_examples, batch_size=200, disc_iter=2,
-                 learning_rate=.005,lambda_gp=10):
+                 learning_rate=.005, lambda_gp=10):
+        """
+        Initializes the WGAN model with a generator, discriminator,
+        latent generator, and real examples.
+        """
         super().__init__()  # run the __init__ of keras.Model first.
         self.latent_generator = latent_generator
-        self.real_examples    = real_examples
-        self.generator        = generator
-        self.discriminator    = discriminator
-        self.batch_size       = batch_size
-        self.disc_iter        = disc_iter
-        
-        self.learning_rate    = learning_rate
-        self.beta_1=.3   # standard value, but can be changed if necessary
-        self.beta_2=.9   # standard value, but can be changed if necessary
-        
+        self.real_examples = real_examples
+        self.generator = generator
+        self.discriminator = discriminator
+        self.batch_size = batch_size
+        self.disc_iter = disc_iter
+
+        self.learning_rate = learning_rate
+        self.beta_1 = .3   # standard value, but can be changed if necessary
+        self.beta_2 = .9   # standard value, but can be changed if necessary
+
         self.lambda_gp = lambda_gp
         self.dims = self.real_examples.shape
-        self.len_dims=tf.size(self.dims)
+        self.len_dims = tf.size(self.dims)
         self.axis = tf.range(1, self.len_dims, delta=1, dtype='int32')
-        self.scal_shape=self.dims.as_list()
-        self.scal_shape[0]=self.batch_size
-        for i in range(1,self.len_dims):
-            self.scal_shape[i]=1
-        self.scal_shape=tf.convert_to_tensor(self.scal_shape)
-        
+        self.scal_shape = self.dims.as_list()
+        self.scal_shape[0] = self.batch_size
+        for i in range(1, self.len_dims):
+            self.scal_shape[i] = 1
+        self.scal_shape = tf.convert_to_tensor(self.scal_shape)
+
         # define the generator loss and optimizer:
-        self.generator.loss  = lambda x : -tf.reduce_mean(x)               
-        self.generator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.generator.compile(optimizer=generator.optimizer , loss=generator.loss )
-        
+        self.generator.loss = lambda x: -tf.reduce_mean(x)
+        self.generator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate, beta_1=self.beta_1,
+            beta_2=self.beta_2)
+        self.generator.compile(optimizer=generator.optimizer,
+                               loss=generator.loss)
+
         # define the discriminator loss and optimizer:
-        self.discriminator.loss = lambda x , y : tf.reduce_mean(y) - tf.reduce_mean(x) 
-        self.discriminator.optimizer = keras.optimizers.Adam(learning_rate=self.learning_rate, beta_1=self.beta_1, beta_2=self.beta_2)
-        self.discriminator.compile(optimizer=discriminator.optimizer , loss=discriminator.loss )
+        self.discriminator.loss = lambda x, y: (
+            tf.reduce_mean(y) - tf.reduce_mean(x))
+        self.discriminator.optimizer = keras.optimizers.Adam(
+            learning_rate=self.learning_rate,
+            beta_1=self.beta_1, beta_2=self.beta_2)
+        self.discriminator.compile(optimizer=discriminator.optimizer,
+                                   loss=discriminator.loss)
 
     # generator of real samples of size batch_size
     def get_fake_sample(self, size=None, training=False):
-        if not size :
-            size= self.batch_size
+        """
+        Generates a batch of fake samples using the generator.
+        """
+        if not size:
+            size = self.batch_size
         return self.generator(self.latent_generator(size), training=training)
 
     # generator of fake samples of size batch_size
     def get_real_sample(self, size=None):
-        if not size :
-            size= self.batch_size
+        """
+        Retrieves a batch of real samples from the dataset.
+        """
+        if not size:
+            size = self.batch_size
         sorted_indices = tf.range(tf.shape(self.real_examples)[0])
-        random_indices  = tf.random.shuffle(sorted_indices)[:size]
+        random_indices = tf.random.shuffle(sorted_indices)[:size]
         return tf.gather(self.real_examples, random_indices)
-    
+
     # generator of interpolating samples of size batch_size
-    def get_interpolated_sample(self,real_sample,fake_sample):
+    def get_interpolated_sample(self, real_sample, fake_sample):
         u = tf.random.uniform(self.scal_shape)
-        v=tf.ones(self.scal_shape)-u
+        v = tf.ones(self.scal_shape)-u
         return u*real_sample+v*fake_sample
-    
+
     # computing the gradient penalty
-    def gradient_penalty(self,interpolated_sample):
+    def gradient_penalty(self, interpolated_sample):
         with tf.GradientTape() as gp_tape:
-                gp_tape.watch(interpolated_sample)
-                pred = self.discriminator(interpolated_sample, training=True)
+            gp_tape.watch(interpolated_sample)
+            pred = self.discriminator(interpolated_sample, training=True)
         grads = gp_tape.gradient(pred, [interpolated_sample])[0]
         norm = tf.sqrt(tf.reduce_sum(tf.square(grads), axis=self.axis))
-        return tf.reduce_mean((norm - 1.0) ** 2)      
-     
-         # overloading train_step()    
+        return tf.reduce_mean((norm - 1.0) ** 2)
+
+    # overloading train_step()
     def train_step(self, useless_argument):
         """
         Performs one training step for the WGAN model using weight clipping.
