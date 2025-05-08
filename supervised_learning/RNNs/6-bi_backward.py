a97e3823f4857f42edd80bf71d7cfd682aa4dd53
essioui <salahessioui@gmail.com>
2025-05-08 22:33:30 +0100

    Bidirectional Cell Backward

---
+++ supervised_learning/RNNs/6-bi_backward.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Bidirectional Cell Forward
+"""
+import numpy as np
+
+
+class BidirectionalCell:
+    """
+    Represents a bidirectional cell of an RNN
+    """
+    def __init__(self, i, h, o):
+        """
+        Class constructor
+        Args:
+            i is the dimensionality of the data
+            h is the dimensionality of the hidden states
+            o is the dimensionality of the outputs
+        Attributes:
+            Whf and bhf are for the hidden states in the forward direction
+            Whb and bhb are for the hidden states in the backward direction
+            Wy and by are for the outputs
+        """
+        self.Whf = np.random.randn(i + h, h)
+        self.bhf = np.zeros((1, h))
+
+        self.Whb = np.random.randn(i + h, h)
+        self.bhb = np.zeros((1, h))
+
+        self.Wy = np.random.randn(2 * h, o)
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """
+        Calculates the hidden state in the forward direction
+        Args:
+            x_t is a numpy.ndarray of shape (m, i)
+                m is the batch size for the data
+            h_prev is a numpy.ndarray of shape (m, h)
+        Returns:
+            h_next: the next hidden state
+        """
+        concat = np.concatenate((h_prev, x_t), axis=1)
+
+        h_next = np.tanh(
+            np.matmul(concat, self.Whf) + self.bhf
+        )
+
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """
+        Calculates the hidden state in the backward direction
+        Args:
+            x_t is a numpy.ndarray of shape (m, i)
+                m is the batch size for the data
+            h_next is a numpy.ndarray of shape (m, h)
+        Returns:
+            h_pev: the previous hidden state
+        """
+        concat = np.concatenate((h_next, x_t), axis=1)
+
+        h_prev = np.tanh(
+            np.matmul(concat, self.Whb) + self.bhb
+        )
+
+        return h_prev
