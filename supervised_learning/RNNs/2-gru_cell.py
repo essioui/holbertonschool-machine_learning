8446ff9b34fd3d23242ab247dd61a421648f5b66
essioui <salahessioui@gmail.com>
2025-05-08 13:52:24 +0100

    Gated Recurrent Unit (GRU)

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -1,31 +1,8 @@
 #!/usr/bin/env python3
 """
-gated recurrent unit GRU Cell
+Gated Recurrent Unit (GRU)
 """
 import numpy as np
-
-
-def sigmoid(x):
-    """
-    Sigmoid activation function
-    Args:
-        x: input value
-    Returns:
-        Sigmoid of x
-    """
-    return 1 / (1 + np.exp(-x))
-
-
-def softmax(x):
-    """
-    Softmax activation function
-    Args:
-        x: input value
-    Returns:
-        Softmax of x
-    """
-    e_x = np.exp(x - np.max(x, axis=1, keepdims=True))
-    return e_x / e_x.sum(axis=1, keepdims=True)
 
 
 class GRUCell:
@@ -45,16 +22,13 @@ class GRUCell:
             Wh and bh are for the intermediate hidden state
             Wy and by are for the output
         """
-        self.Wz = np.random.randn(i, h)
-        self.Uz = np.random.randn(h, h)
+        self.Wz = np.random.randn(i + h, h)
         self.bz = np.zeros((1, h))
 
-        self.Wr = np.random.randn(i, h)
-        self.Ur = np.random.randn(h, h)
+        self.Wr = np.random.randn(i + h, h)
         self.br = np.zeros((1, h))
 
-        self.Wh = np.random.randn(i, h)
-        self.Uh = np.random.randn(h, h)
+        self.Wh = np.random.randn(i + h, h)
         self.bh = np.zeros((1, h))
 
         self.Wy = np.random.randn(h, o)
@@ -70,15 +44,52 @@ class GRUCell:
             h_next is the next hidden state
             y_t is the output of the cell
         """
-        z = sigmoid(np.dot(x_t, self.Wz) + np.dot(h_prev, self.Uz) + self.bz)
+        # Concatenate previous hidden state and current input
+        concat_h_x = np.concatenate((h_prev, x_t), axis=1)
 
-        r = sigmoid(np.dot(x_t, self.Wr) + np.dot(h_prev, self.Ur) + self.br)
+        # Update gate
+        z_t = self.sigmoid(np.dot(concat_h_x, self.Wz) + self.bz)
 
-        h_tilde = np.tanh(np.dot(x_t, self.Wh) + np.dot(
-            r * h_prev, self.Uh) + self.bh)
+        # Reset gate
+        r_t = self.sigmoid(np.dot(concat_h_x, self.Wr) + self.br)
 
-        h_next = (1 - z) * h_prev + z * h_tilde
+        # Intermediate hidden state
+        concat_r_h_x = np.concatenate((h_prev * r_t, x_t), axis=1)
 
-        y = softmax(np.dot(h_next, self.Wy) + self.by)
+        # h_hat_t
+        h_hat_t = np.tanh(np.dot(concat_r_h_x, self.Wh) + self.bh)
 
+        # Final hidden state
+        h_next = (1 - z_t) * h_prev + z_t * h_hat_t
+
+        # Output
+        y_t = np.dot(h_next, self.Wy) + self.by
+
+        # Softmax activation
+        y = self.softmax(y_t)
+
+        # Return the next hidden state and the output
         return h_next, y
+
+    @staticmethod
+    def sigmoid(x):
+        """
+        Sigmoid activation function
+        Args:
+            x is the input data
+        Returns:
+            The sigmoid of x
+        """
+        return 1 / (1 + np.exp(-x))
+
+    @staticmethod
+    def softmax(x):
+        """
+        Softmax activation function
+        Args:
+            x is the input data
+        Returns:
+            The softmax of x
+        """
+        exp_x = np.exp(x - np.max(x))
+        return exp_x / exp_x.sum(axis=1, keepdims=True)
