62d14c6e18776d9935e968ff5d9962d496d02850
essioui <salahessioui@gmail.com>
2025-06-19 00:29:50 +0100

    Transformer Decoder

--- supervised_learning/attention/10-transformer_decoder.py
+++ supervised_learning/attention/10-transformer_decoder.py
@@ -2,8 +2,8 @@
 """
 Transformer Decoder
 """
+
 import tensorflow as tf
-import numpy as np
 positional_encoding = __import__('4-positional_encoding').positional_encoding
 DecoderBlock = __import__('8-transformer_decoder_block').DecoderBlock
 
@@ -12,6 +12,7 @@ class Decoder(tf.keras.layers.Layer):
     """
     Transformer Decoder
     """
+
     def __init__(
         self, N, dm, h, hidden, target_vocab, max_seq_len, drop_rate=0.1
     ):
@@ -25,21 +26,18 @@ class Decoder(tf.keras.layers.Layer):
             target_vocab (int): Size of the target vocabulary.
             max_seq_len (int): Maximum sequence length for positional encoding.
             drop_rate (float): Dropout rate.
-        Returns:
-            None
         """
         super(Decoder, self).__init__()
-
         self.dm = dm
         self.N = N
 
         # Embedding layer for target vocabulary
         self.embedding = tf.keras.layers.Embedding(target_vocab, dm)
 
-        # Positional encoding for the decoder
+        # Positional encoding matrix
         self.positional_encoding = positional_encoding(max_seq_len, dm)
 
-        # Create the positional encoding matrix
+        # List of DecoderBlocks
         self.blocks = (
             [DecoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
         )
@@ -61,14 +59,17 @@ class Decoder(tf.keras.layers.Layer):
         """
         seq_len = tf.shape(x)[1]
 
-        # Embedding and positional encoding
+        # Embedding + scaling
         x = self.embedding(x)
         x *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+
+        # Add positional encoding
         x += self.positional_encoding[:seq_len, :]
 
         # Apply dropout
         x = self.dropout(x, training=training)
 
+        # Pass through each decoder block
         for i in range(self.N):
             x = self.blocks[i](
                 x,
@@ -77,4 +78,5 @@ class Decoder(tf.keras.layers.Layer):
                 look_ahead_mask=look_ahead_mask,
                 padding_mask=padding_mask
             )
+
         return x
