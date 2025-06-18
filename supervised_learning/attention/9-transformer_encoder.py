e053ae58a1f4dd3478b071d26508927eb8d2fd55
essioui <salahessioui@gmail.com>
2025-06-19 00:13:21 +0100

    pycodestyle

--- supervised_learning/attention/9-transformer_encoder.py
+++ supervised_learning/attention/9-transformer_encoder.py
@@ -12,7 +12,22 @@ class Encoder(tf.keras.layers.Layer):
     """
     Transformer Encoder
     """
-    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1):
+    def __init__(
+        self, N, dm, h, hidden, input_vocab, max_seq_len, drop_rate=0.1
+    ):
+        """
+        Initializes the Encoder.
+        Args:
+            N (int): Number of blocks in the encoder.
+            dm (int): Dimensionality of the model.
+            h (int): Number of heads in the multi-head attention.
+            hidden (int): Dimensionality of the feed-forward network.
+            input_vocab (int): Size of the input vocabulary.
+            max_seq_len (int): Maximum sequence length for positional encoding.
+            drop_rate (float): Dropout rate.
+        Returns:
+            None
+        """
         super(Encoder, self).__init__()
         self.dm = dm
         self.N = N
@@ -20,10 +35,21 @@ class Encoder(tf.keras.layers.Layer):
         self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
         self.positional_encoding = positional_encoding(max_seq_len, dm)
 
-        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        self.blocks = (
+            [EncoderBlock(dm, h, hidden, drop_rate) for _ in range(N)]
+        )
         self.dropout = tf.keras.layers.Dropout(drop_rate)
 
     def call(self, x, training, padding_mask):
+        """
+        Forward pass of the Encoder.
+        Args:
+            x (tf.Tensor): Input tensor of shape (batch_size, seq_len).
+            training (bool): Whether the model is in training mode.
+            padding_mask (tf.Tensor): Padding mask tensor.
+        Returns:
+            tf.Tensor: Output tensor of shape (batch_size, seq_len, dm).
+        """
         seq_len = tf.shape(x)[1]
 
         x = self.embedding(x)
