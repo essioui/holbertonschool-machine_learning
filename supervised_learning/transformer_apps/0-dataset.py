0d6d98ffa5b14b78724d01e8b878e0183232d20e
essioui <salahessioui@gmail.com>
2025-06-20 00:20:59 +0100

    0-dataset

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -9,32 +9,27 @@ class Dataset:
 
     def __init__(self):
         """Initialize the dataset and create tokenizers."""
-        # Load dataset from TensorFlow Datasets
+        # Load train and validation splits separately
         self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
-                                    split='validation', as_supervised=True)
+                                    split='train', as_supervised=True)
         self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
                                     split='validation', as_supervised=True)
 
-        # Build the tokenizers
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
-            self.data_train
-        )
+        # Build the tokenizers (using train data only if you want, here just loading pretrained)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset()
 
-    def tokenize_dataset(self, data):
+    def tokenize_dataset(self):
         """
-        Creates sub-word tokenizers for our dataset using BERT Fast tokenizers.
-
-        Args:
-            data: tf.data.Dataset in the format (pt, en)
+        Load pretrained BERT Fast tokenizers for Portuguese and English.
 
         Returns:
             tokenizer_pt: BERT Fast tokenizer for Portuguese
             tokenizer_en: BERT Fast tokenizer for English
         """
         tokenizer_pt = transformers.BertTokenizerFast.from_pretrained(
-            'neuralmind/bert-base-portuguese-cased'
+            'neuralmind/bert-base-portuguese-cased', use_fast=True
         )
         tokenizer_en = transformers.BertTokenizerFast.from_pretrained(
-            'bert-base-uncased'
+            'bert-base-uncased', use_fast=True
         )
         return tokenizer_pt, tokenizer_en
