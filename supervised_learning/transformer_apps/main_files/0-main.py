b05f5c9c482d17f13bb5cf7966b089dd342d0975
essioui <salahessioui@gmail.com>
2025-06-20 00:02:32 +0100

    "Dataset class for machine translation

--- supervised_learning/transformer_apps/main_files/0-main.py
+++ supervised_learning/transformer_apps/main_files/0-main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import sys
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 Dataset = __import__('0-dataset').Dataset
