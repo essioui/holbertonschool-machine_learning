f6691170dc72a6a9b6c26a4f4c2456237631e8ce
essioui <salahessioui@gmail.com>
2025-06-20 17:11:00 +0100

    Encode Tokens

---
+++ supervised_learning/transformer_apps/main_files/1-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+Dataset = __import__('1-dataset').Dataset
+
+data = Dataset()
+for pt, en in data.data_train.take(1):
+    print(data.encode(pt, en))
+for pt, en in data.data_valid.take(1):
+    print(data.encode(pt, en))
