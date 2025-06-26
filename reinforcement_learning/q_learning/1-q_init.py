a25309c488b3fc2cf5b8934ba9d33c9479efbb30
essioui <salahessioui@gmail.com>
2025-06-27 00:14:41 +0100

    Initialize Q-table

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Initialize Q-table
+"""
+import gymnasium as gym
+import numpy as np
+
+
+def q_init(env):
+    """
+    Initialize the Q-table for the given environment.
+    Args:
+        env (gym.Env): The environment to initialize the Q-table for.
+    Returns:
+        np.ndarray: The initialized Q-table with shape (n_states, n_actions).
+    """
+    n_states = env.observation_space.n
+    n_actions = env.action_space.n
+    Q = np.zeros((n_states, n_actions))
+
+    return Q
