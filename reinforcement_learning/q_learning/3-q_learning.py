a6a878a09254c482dd162271f9fe3167297bdc7e
essioui <salahessioui@gmail.com>
2025-06-27 01:22:17 +0100

    train Q-learning

--- reinforcement_learning/q_learning/3-q_learning.py
+++ reinforcement_learning/q_learning/3-q_learning.py
@@ -12,6 +12,19 @@ def train(
 ):
     """
     Train the agent using Q-learning algorithm
+    Args:
+        env is the FrozenLakeEnv instance
+        Q is a numpy.ndarray containing the Q-table
+        episodes is the total number of episodes to train over
+        max_steps is the maximum number of steps per episode
+        alpha is the learning rate
+        gamma is the discount rate
+        epsilon is the initial threshold for epsilon greedy
+        min_epsilon is the minimum value that epsilon should decay to
+        epsilon_decay is the decay rate for updating epsilon between episodes
+    Returns: Q, total_rewards
+        Q is the updated Q-table
+        total_rewards is a list containing the rewards per episode
     """
     total_rewards = []
 
