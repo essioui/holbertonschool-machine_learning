ddf4a8e30dcdcb65550a3479f3af1eed1ef34a63
essioui <salahessioui@gmail.com>
2025-07-24 18:35:30 +0100

    Slice

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Slice
+"""
+
+
+def slice(df):
+    """
+    Takes a pd.DataFrame and:
+        Extracts the columns High, Low, Close, and Volume_BTC.
+        Selects every 60th row from these columns.
+    Returns:
+        the sliced pd.DataFrame
+    """
+    df1 = df[['High', 'Low', 'Close', 'Volume_(BTC)']]
+
+    df2 = df1.iloc[::60, :]
+
+    return df2.reset_index(drop=True)
