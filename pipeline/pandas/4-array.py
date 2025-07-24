b897701d0103b868821c9729173bbdb3f69c5968
essioui <salahessioui@gmail.com>
2025-07-24 18:05:20 +0100

    To Numpy

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+To Numpy
+"""
+import pandas as pd
+
+
+def array(df):
+    """
+    Takes a pd.DataFrame as input and performs the following:
+        df is a pd.DataFrame containing columns named High and Close
+        should select the last 10 rows of the High and Close columns.
+        Convert these selected values into a numpy.ndarray.
+    Returns:
+        the numpy.ndarray
+    """
+    df1 = df.iloc[-10:][['High', 'Close']]
+
+    # or we can convert by arr = df1.values
+    return df1.to_numpy()
---
+++ pipeline/pandas/main_files/4-main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from_file = __import__('2-from_file').from_file
+array = __import__('4-array').array
+
+df = from_file('../datasets/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
+
+A = array(df)
+
+print(A)
