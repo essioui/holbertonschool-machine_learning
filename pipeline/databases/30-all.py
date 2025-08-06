5c99535e822c29f0f774a350c8a1a3ab11125e9b
essioui <salahessioui@gmail.com>
2025-08-06 18:36:10 +0100

    List all documents in Python

--- pipeline/databases/30-all.py
+++ pipeline/databases/30-all.py
@@ -1,10 +1,12 @@
-#!usr/bin/env python3
+#!/usr/bin/env python3
+"""
+List all documents in Python
+"""
 
 def list_all(mongo_collection):
     """
     Return all documents in the given MongoDB collection.
     If no documents found, returns an empty list.
-    
     Args:
         mongo_collection: pymongo collection object
 
