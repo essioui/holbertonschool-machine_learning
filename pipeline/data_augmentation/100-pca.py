455e7344bbd5d3599ca09f38505079adbc7ed161
essioui <salahessioui@gmail.com>
2025-08-15 00:29:47 +0100

    PCA

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -16,25 +16,34 @@ def pca_color(image, alphas):
     Returns:
         tf.Tensor: Augmented image of same shape and dtype uint8
     """
+    # Normalize the image to [0, 1]
     image = tf.cast(image, tf.float32) / 255.0
-    orig_shape = tf.shape(image)
-    flat_image = tf.reshape(image, [-1, 3])
 
-    mean = tf.reduce_mean(flat_image, axis=0)
-    centered = flat_image - mean
+    # Reshape the image to (pixel_count, 3)
+    flat_image = tf.reshape(image, (-1, 3))
 
-    cov = tf.matmul(
-        tf.transpose(centered), centered) / tf.cast(
-            tf.shape(flat_image)[0], tf.float32
-        )
-    eigvals, eigvecs = tf.linalg.eigh(cov)
+    # Compute the mean and center the image
+    mean = tf.reduce_mean(flat_image, axis=0, keepdims=True)
+    centered_image = flat_image - mean
 
-    alphas = tf.constant(alphas, dtype=tf.float32)
-    delta = tf.matmul(eigvecs, tf.reshape(eigvals * alphas, [3, 1]))
-    delta = tf.reshape(delta, [1, 3])
+    # Compute the covariance matrix
+    covariance_matrix = \
+        tf.matmul(tf.transpose(centered_image),
+                  centered_image) / tf.cast(tf.shape(flat_image)[0],
+                                            tf.float32)
 
-    augmented = flat_image + delta
-    augmented = tf.clip_by_value(augmented, 0, 1)
-    augmented = tf.reshape(augmented, orig_shape)
+    # Compute eigenvalues and eigenvectors
+    eigenvalues, eigenvectors = tf.linalg.eigh(covariance_matrix)
 
-    return tf.cast(augmented * 255, tf.uint8)
+    # Adjust colors using eigenvalues and alphas
+    delta = tf.matmul(eigenvectors, tf.reshape(eigenvalues * alphas, [-1, 1]))
+    delta = tf.transpose(delta)
+    delta = tf.broadcast_to(delta, tf.shape(centered_image))
+    augmented_image = centered_image + delta + mean
+
+    # Clip values to [0, 1] and reshape back to original shape
+    augmented_image = tf.clip_by_value(augmented_image, 0, 1)
+    augmented_image = tf.reshape(augmented_image, tf.shape(image))
+
+    # Convert back to tensor and scale to [0, 255]
+    return tf.cast(augmented_image * 255, tf.uint8)
