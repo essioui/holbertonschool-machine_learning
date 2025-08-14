18fa4b1c68d57fda8f5d94a8caa7f2dd0cdad928
essioui <salahessioui@gmail.com>
2025-08-15 00:22:22 +0100

    PCA Color

--- pipeline/data_augmentation/100-pca.py
+++ pipeline/data_augmentation/100-pca.py
@@ -4,6 +4,7 @@ PCA Color Augmentation
 """
 import tensorflow as tf
 
+
 def pca_color(image, alphas):
     """
     Performs PCA color augmentation on an image as in AlexNet paper.
@@ -17,17 +18,20 @@ def pca_color(image, alphas):
     """
     image = tf.cast(image, tf.float32) / 255.0
     orig_shape = tf.shape(image)
-    flat_image = tf.reshape(image, [-1,3])
+    flat_image = tf.reshape(image, [-1, 3])
 
     mean = tf.reduce_mean(flat_image, axis=0)
     centered = flat_image - mean
 
-    cov = tf.matmul(tf.transpose(centered), centered) / tf.cast(tf.shape(flat_image)[0], tf.float32)
+    cov = tf.matmul(
+        tf.transpose(centered), centered) / tf.cast(
+            tf.shape(flat_image)[0], tf.float32
+        )
     eigvals, eigvecs = tf.linalg.eigh(cov)
 
     alphas = tf.constant(alphas, dtype=tf.float32)
-    delta = tf.matmul(eigvecs, tf.reshape(eigvals * alphas, [3,1]))
-    delta = tf.reshape(delta, [1,3])
+    delta = tf.matmul(eigvecs, tf.reshape(eigvals * alphas, [3, 1]))
+    delta = tf.reshape(delta, [1, 3])
 
     augmented = flat_image + delta
     augmented = tf.clip_by_value(augmented, 0, 1)
